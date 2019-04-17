11423168b53415faedfd796113f11b2092910d3e
Jian Li <kdlijian@gmail.com>
2019-04-17 15:00:27 +0800

    vendored

--- demo/main.go
+++ demo/main.go
@@ -1,32 +1,38 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
-	"log"
 	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-type myhandler struct {
-}
+// Handler
+func hello(c echo.Context) error {
+	version := os.Getenv("VERSION")
+	hostname, err := os.Hostname()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "get hostname error")
+	}
 
-func (h *myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println("received a request")
-	fmt.Fprintf(w, "Hello, I am an ingress-controller demo!\n")
+	return c.JSON(http.StatusOK, echo.Map{
+		"version":  version,
+		"hostname": hostname,
+	})
 }
 
 func main() {
-	s := &http.Server{
-		Addr:      ":8080",
-		Handler:   &myhandler{},
-		TLSConfig: &tls.Config{
-			// ServerName: "www.simple.org",
-		},
-	}
+	// Echo instance
+	e := echo.New()
 
-	err := s.ListenAndServeTLS("../certs/simple.org.crt", "../certs/simple.org.key")
-	if err != nil {
-		fmt.Println("ListenAndServeTLS err:", err)
-	}
-	fmt.Println("ListenAndServeTLS exit ok")
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.StartTLS(":443", "./certs/simple.org.crt", "./certs/simple.org.key"))
 }
